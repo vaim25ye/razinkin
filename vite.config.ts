42473f4f9bb27916d4166b094eb5d39f320b7471
vaim25ye <93862283+vaim25ye@users.noreply.github.com>
2025-12-16 16:37:09 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
   build: {
     outDir: 'dist',
     sourcemap: false
